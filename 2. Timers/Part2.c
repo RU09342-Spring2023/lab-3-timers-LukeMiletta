3704caa978b740104fb0d06b041577cfec11c300
Luciano Miletta <milett56@students.rowan.edu>
2023-02-21 17:40:14 -0500

    Completed
    I have successfully completed Lab 3 part 2

--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -1,67 +1,88 @@
-/*
- * Part2.c
- *
- *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
- *
- *      This code will need to change the speed of an LED between 3 different speeds by pressing a button.
- */
+
+//   Part2.c
+//  Created on: Feb211, 2023
+//   Author: Luciano Miles Mileeea
 
 #include <msp430.h>
-
+int count=0;
 void gpioInit();
 void timerInit();
+
 
 void main(){
 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
 
-    gpioInit();
+    gpioInit();                             // Disable the GPIO power-on default high-impedance mode
     timerInit();
-
-    // Disable the GPIO power-on default high-impedance mode
-    // to activate previously configured port settings
     PM5CTL0 &= ~LOCKLPM5;
 
     __bis_SR_register(LPM3_bits | GIE);
 
 }
 
+char REDLED = 0x01;
 
-void gpioInit(){
-    // @TODO Initialize the Red or Green LED
+void gpioInit()
 
-    // @TODO Initialize Button 2.3
+{
 
+
+    P6OUT &= ~BIT6;                         // Clear P6.6 output latch for a defined power-on state
+    P6DIR |= BIT6;                          // Set P6.6 to output direction
+
+
+
+    P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+    P2REN |= BIT3;                          // P2.3 pull-up register enable
+    P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+    P2IE |= BIT3;                           // P2.3 interrupt enabled
+    P2IFG &= ~BIT3;                         // Clear P1.3 IFG
 
 }
 
-void timerInit(){
-    // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
+void timerInit()
+
+{
+    TB1CCTL0 = CCIE;                          // TBCCR0 interrupt enabled
+    TB1CCR0 = 5000;
+    TB1CTL = TBSSEL_1 | MC_2;                 // ACLK, continuous mode
 
 }
 
 
-/*
- * INTERRUPT ROUTINES
- */
-
-// Port 2 interrupt service routine
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
+    count ++;                                 // Add 1 to the counter each time
+    P6OUT^= BIT6;                             // Turn on Green Light
+    P2IFG&= ~BIT3;                            // Clear P1.3 IFG
 
-    // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
 
 }
+
 
 
 // Timer B1 interrupt service routine
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR(void)
 {
-    // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+    if (count == 1)                           // If Count Equals 1
+            {
+    TB1CCR0 += 5000;                          // Make Led Blink fast
+    P6OUT ^= BIT6;                            // Make Led Blink
+            }
+    else if (count == 2)                      // If count equals 2
+            {
+    TB1CCR0 += 10000;                         // Make LED blink at a medium Pace
+    P6OUT ^= BIT6;                            // Make the Green Led blink
+                    }
+    else if (count == 3)
+            {
+    TB1CCR0 += 15000;                         // Make the LED blink at a longer pace
+    P6OUT ^= BIT6;                            // Make the Green LED blink
+                    }
+    else                                      // if count isnt a 1,2 or a 3
+        count = 0;                            // Make Count = 0
+
 }
-
-
